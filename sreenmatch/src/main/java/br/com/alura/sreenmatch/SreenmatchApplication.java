614753bf91d1546a000cb76d2091921a2d83a3c9
João Lucas <jlucassantosramos@gmail.com>
2024-04-29 16:43:23 -0300

    Primeiro Commit Spring

---
+++ sreenmatch/src/main/java/br/com/alura/sreenmatch/SreenmatchApplication.java
@@ -0,0 +1,26 @@
+package br.com.alura.sreenmatch;
+
+import br.com.alura.sreenmatch.Service.ConsumoApi;
+import br.com.alura.sreenmatch.Service.ConverteDados;
+import br.com.alura.sreenmatch.model.DadosSerie;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SreenmatchApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SreenmatchApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		ConsumoApi consumoApi = new ConsumoApi();
+		var json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=620fad6f");
+		System.out.println(json);
+		ConverteDados conversor = new ConverteDados();
+		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
+		System.out.println(dados);
+	}
+}
